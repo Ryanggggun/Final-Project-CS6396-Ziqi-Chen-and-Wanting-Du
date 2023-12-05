b47e180fbce510b702c4c90589ee6795690f7d81
Ryanggggun <120218984+Ryanggggun@users.noreply.github.com>
2023-12-04 20:22:28 -0500

    Update reply_generator.py

--- UI System/reply_generator.py
+++ UI System/reply_generator.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = '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'
+openai.api_key = ''
 
 def get_chatgpt_response(comment, product_url, model="gpt-4-1106-preview", max_tokens=60):
     prompt = f"A customer has a concern about the product: '{comment}'. Imagine that you are a competitor of that product and sell the benefits of your own product based on the claim. Don't include sorry and thank you's. Answer in a positive sunny way! don't apologize, and present the benefits of the product(For example, it's cost-effective, very good quality and looks great. Respond to reviews based on their claims). End the response with a link to the product website for additional support."
